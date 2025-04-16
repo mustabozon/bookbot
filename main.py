df461f50a3eceb1d1251ceebbd84a4d27bae443a
mustabozon <sphere.railcar@gmail.com>
2025-04-16 16:58:23 +0400

    Added a dictionary with a character count

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from stats import get_num_words
+from stats import count_char
 
 
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = get_num_words(text)
+    chars_dict = count_char(text)
     print(f"{num_words} words found in the document")
+    print(chars_dict)
 
 
 def get_book_text(path):
